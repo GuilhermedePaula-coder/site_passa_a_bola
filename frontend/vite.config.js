8982e6f1ef5331bda964516f77b7548b95aeaa66
Enzo Ferreira <enzocafaria@gmail.com>
2025-10-22 16:54:11 -0300

    ajustanto package e vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+//  Caminho  para funcionar no localhost e no deploy (Vercel / GitHub Pages)
 export default defineConfig({
   plugins: [react()],
-  base: '/site_passa_a_bola/', // 👈 coloque o nome do repositório aqui
-})
+  base: "./",
+});
