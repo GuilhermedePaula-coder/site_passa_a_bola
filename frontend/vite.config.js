a499d58bd9ffc68647851d306420cfe80342efbe
Enzo Ferreira <enzocafaria@gmail.com>
2025-11-06 15:52:04 -0300

    ajustes vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/site_passa_a_bola/" : "/",
+  base: "./",
   server: {
     port: 5173,
     open: true,
